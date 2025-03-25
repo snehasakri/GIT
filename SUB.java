808e60842ff8b6c1b57217aac41d638f02ed5d3f
Sneha vijaykumar sakri <snehasakri75@gmail.com>
2025-03-25 19:25:25 +0530

    adding message in SUB.java

--- SUB.java
+++ SUB.java
@@ -2,7 +2,7 @@ public class sub{
     public static void main(String[]args){
         int a=4;
         int b=2;
-        System.out.println(a-b);
+        System.out.println("the result is:"+(a-b));
         
     }
 }
